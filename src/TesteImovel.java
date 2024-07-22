5c232d01dcc583c2390203ecfeb9672097f8cc3d
Rakoski <mastrakoski@gmail.com>
2024-07-21 21:46:12 -0300

    imovel'

---
+++ src/TesteImovel.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TesteImovel {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Endereço:");
+        String enderecoScanner = scanner.nextLine();
+        System.out.println("Valor venal:");
+        double valorVendalScanner = scanner.nextDouble();
+
+        Imovel imovel = new Imovel(enderecoScanner, valorVendalScanner);
+        imovel.imprimirDados();
+
+        scanner.close();
+    }
+}
